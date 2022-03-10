00fa3388a70d4cd7ccbb66a5ac36e96d93d68497
uche <travzki@gmail.com>
2022-03-10 09:32:20 +0000

    external db created

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
-#import pymysql
+import pymysql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///proj.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:test@35.189.116.127/qa'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('secret_key')
 
