311162c95ae6b7564853406a4e0b5f0358ce8616
uche <travzki@gmail.com>
2022-03-21 13:10:50 +0000

    update

--- application/__init__.py
+++ application/__init__.py
@@ -4,10 +4,12 @@ from os import getenv
 import pymysql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:test@35.189.116.127/qa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qa.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:test@35.189.116.127/qa'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('secret_key')
 
 db = SQLAlchemy(app)
 
 import application.routes
+
